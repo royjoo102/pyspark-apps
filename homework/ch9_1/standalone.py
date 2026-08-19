12b85b7adc91bd48990ccfa15f4f8924a0b5342a
Roy Joo <royjoo102@gmail.com>
2026-08-19 15:02:21 -0700

    completed hw

---
+++ homework/ch9_1/standalone.py
@@ -0,0 +1,15 @@
+from pyspark.sql import SparkSession
+import time
+
+
+spark = SparkSession \
+    .builder \
+    .appName('simple_pyspark') \
+    .getOrCreate()
+
+schema = 'ID INT, COUNTRY STRING, HIT LONG'
+df = spark.createDataFrame(data=[(1,'Korea', 120),(2,'USA', 80), (3,'Japna', 40)], schema=schema)
+df.show()
+
+# sleep 5 minute
+time.sleep(6000)
