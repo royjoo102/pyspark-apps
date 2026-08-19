f83b1e9abb2eb984ad8693e9bd67a236a64968dc
Roy Joo <royjoo102@gmail.com>
2026-08-19 15:04:18 -0700

    Added df.count()

--- homework/ch9_1/standalone.py
+++ homework/ch9_1/standalone.py
@@ -10,6 +10,7 @@ spark = SparkSession \
 schema = 'ID INT, COUNTRY STRING, HIT LONG'
 df = spark.createDataFrame(data=[(1,'Korea', 120),(2,'USA', 80), (3,'Japna', 40)], schema=schema)
 df.show()
+df.count()
 
 # sleep 5 minute
 time.sleep(6000)
