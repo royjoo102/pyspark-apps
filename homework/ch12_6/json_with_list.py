5a7156c9b2b98013fa64549b99405915cfa1ef01
Roy Joo <royjoo102@gmail.com>
2026-08-27 20:36:28 -0700

    updated

--- homework/ch12_6/json_with_list.py
+++ homework/ch12_6/json_with_list.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.dataframe import DataFrame
-from pyspark.sql.functions import from_json, col
+from pyspark.sql.functions import from_json, col, explode
 from pyspark.sql.types import IntegerType, StringType, StructType, StructField, ArrayType
 
 
@@ -14,9 +14,10 @@ schema = StructType(
             ]
         ), False),
         StructField('age', IntegerType(), False),
-        StructField('hobbies',ArrayType(StringType(),True),True)
+        StructField('hobbies', ArrayType(StringType()), False)
     ]
 )
+
 
 def for_each_batch_func(df: DataFrame, epoch_id):
     print(f'====================================== epoch_id: {epoch_id} start ======================================')
@@ -25,29 +26,30 @@ def for_each_batch_func(df: DataFrame, epoch_id):
 
     df.show(truncate=False)
 
-    from_json_df = df.select(
+    json_to_col_df = df.select(
         from_json(col('VALUE'), schema).alias('person_info')
-    )
-    from_json_df.printSchema()
-    json_schema=from_json_df.schema
-    print(json_schema)
+    ).select('person_info.*') \
+     .selectExpr('name',
+                 'address.*',
+                 'CAST(age AS INT) AS age',
+                 'hobbies')
 
-    print(f'from_json_df.show()')
-    from_json_df.show(truncate=False)
-
-    json_to_col_df = from_json_df.select('person_info.*') \
-                                 .selectExpr('name',
-                                             'address.*',
-                                             'CAST(age AS INT) AS age ',
-                                             'hobbies')
     print(f'json_to_col_df.show()')
     json_to_col_df.show(truncate=False)
+    exploded_df = json_to_col_df.select('name',
+                                        'country',
+                                        'city',
+                                        'age',
+                                        explode(col('hobbies')).alias('hobbies')
+    )
+    print(f'exploded_df.show()')
+    exploded_df.show(truncate=False)
 
     df.unpersist()
     print(f'====================================== epoch_id: {epoch_id} end ======================================')
 
 
-app_name = 'from_json'
+app_name = 'json_with_list'
 spark = SparkSession \
         .builder \
         .appName(app_name) \
@@ -58,7 +60,6 @@ kafka_source_df = spark.readStream \
                 .option("kafka.bootstrap.servers", "kafka01:9092,kafka02:9092,kafka03:9092") \
                 .option("subscribe", "lesson.spark-streaming.person_info2") \
                 .option('startingOffsets','latest') \
-                .option('failOnDataLoss','false') \
                 .load() \
                 .selectExpr(
                     "CAST(key AS STRING) AS KEY",
