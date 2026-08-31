332b2e7135b84954b9f43b15e7427b42999dc01b
Roy Joo <royjoo102@gmail.com>
2026-08-31 15:33:12 -0700

    updated

--- homework/ch16_4/complete_mode.py
+++ homework/ch16_4/complete_mode.py
@@ -5,7 +5,8 @@ from pyspark.sql.types import StructType, StructField, ArrayType, StringType
 from pyspark.sql import SparkSession
 import time
 
-class UpdateMode(BaseStreamApp):
+
+class CompleteMode(BaseStreamApp):
     def __init__(self, app_name):
         super().__init__(app_name)
 
@@ -19,30 +20,31 @@ class UpdateMode(BaseStreamApp):
         spark = self.get_session_builder().getOrCreate()
 
         df = spark.readStream \
-            .format('kafka') \
-            .option('kafka.bootstrap.servers','kafka01:9092,kafka02:9092,kafka03:9092') \
-            .option('subscribe','lesson.ch16_4.output-mode') \
-            .option('maxOffsetsPerTrigger','1') \
-            .load() \
-            .selectExpr('CAST(key AS STRING) AS KEY',
-                        'CAST(value AS STRING) AS VALUE') \
-            .select(from_json(col('VALUE'), schema).alias('VALUE_JSON')) \
-            .select(explode(col('VALUE_JSON.NAME')).alias('NAME')) \
-            .groupBy('NAME').count()
+                .format('kafka') \
+                .option('kafka.bootstrap.servers','kafka01:9092,kafka02:9092,kafka03:9092') \
+                .option('subscribe','lesson.ch16_4.output-mode') \
+                .option('startingOffsets','earliest') \
+                .option('maxOffsetsPerTrigger','1') \
+                .load() \
+                .selectExpr('CAST(key AS STRING) AS KEY',
+                            'CAST(value AS STRING) AS VALUE') \
+                .select(from_json(col('VALUE'), schema).alias('VALUE_JSON')) \
+                .select(explode(col('VALUE_JSON.NAME')).alias('NAME')) \
+                .groupBy('NAME').count()
 
         query = df.writeStream \
-            .foreachBatch(lambda df, epoch: self.for_each_batch(df, epoch, spark)) \
-            .outputMode('complete') \
-            .option("checkpointLocation", self.kafka_offset_dir) \
-            .start()
+                .foreachBatch(lambda df, epoch: self.for_each_batch(df, epoch, spark)) \
+                .outputMode('complete') \
+                .option("checkpointLocation", self.kafka_offset_dir) \
+                .start()
 
         query.awaitTermination()
 
     def _for_each_batch(self, df: DataFrame, epoch_id: int, spark: SparkSession):
         df.show(truncate=False)
-        time.sleep(100)
+        time.sleep(10)
 
 
 if __name__ == '__main__':
-    update_mode = UpdateMode(app_name='update_mode')
-    update_mode.main()
+    complete_mode = CompleteMode(app_name='complete_mode')
+    complete_mode.main()
